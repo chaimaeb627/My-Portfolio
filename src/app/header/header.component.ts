f404c4b40e1d27a78537dca9dbbfc3dafe7c3e9d
chaimaeb627 <chaimaeb627@gmail.com>
2025-03-16 10:03:35 +0000

    uu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,21 @@
-import { Component , HostListener , OnInit } from '@angular/core';
+import { Component , signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isMenuOpen = false;
+  isMenuOpen = signal(false); // Signal pour gérer l'état du menu
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+    this.isMenuOpen.set(!this.isMenuOpen()); // Basculer l'état du menu
   }
 
   closeMenu() {
-    this.isMenuOpen = false;
+    this.isMenuOpen.set(false); // Fermer le menu après un clic sur un lien
   }
-
 }
